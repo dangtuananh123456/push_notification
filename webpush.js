72e9f8b87033aa3b1f4eaa7d4a7e379e2950a480
21120408 <dangtuananh3012003@gmail.com>
2024-11-25 14:36:37 +0700

    thêm  an iOS PWA vào

--- webpush.js
+++ webpush.js
@@ -5,8 +5,8 @@ const webpush = require("web-push");
 // VAPID keys should only be generated only once.
 const vapidKeys = {
   publicKey:
-    "BLd5PIMceWWRzNUF4heJ2-F5gX9yhegZIVuQnesBVaZnLabafWlKAODMw-WG97rZkfah0FKi6jc0AegPd553DUI",
-  privateKey: "znjjUYQiYbxlvrF6x_sP1dmLE2ZlLeDOI6gSqNgX30k",
+    "BMGGpAwp9T7Aw8J5oXsq4Ls0zw_kVwgFgkQ9w23ytvlrq9PcQ1o1hhN9kShZ5weyyllesO2rgIcgVbKGkV6Xf4w",
+  privateKey: "pZ3HcZbp00IrtKUOA4-oKZR4iB04nKKLhP3Robtk6qs",
 };
 
 webpush.setVapidDetails(
@@ -17,21 +17,12 @@ webpush.setVapidDetails(
 
 // This is the same output of calling JSON.stringify on a PushSubscription
 const pushSubscription = {
-  "endpoint": "https://fcm.googleapis.com/fcm/send/fqQlAv6RfXQ:APA91bEpMeJJTTBXosiZOzLvG3lfvJeazam1kZr5K-rZt2QOk3zOxIUeBTTGWk_nljnAyFauMzwPYiApoqfw1CLm8-UPJIMbo4aipOjF-CoJARgh9Q1avp2hmfjJGbkVGjMV6RBaZGzK",
+  "endpoint": "https://fcm.googleapis.com/fcm/send/fSV5QuKQZxI:APA91bG8554ZM7X4vdeD3G1xL5Bb6N4Gll4c034wMSYeDvS5edVAQTS13B9gQXtsqKSPtyqSsawn_4Yn0e5YSkDoLj8lng08h5Tl-Lb2gxc0Q9b2loSWK4TF5OjPZlSHwpCTsfc0nIKf",
   "expirationTime": null,
   "keys": {
-    "p256dh": "BP3yL2xQZX_QNyEMorYT1QyXfQZSgEjBrVY2QcNG21quUqznjDivyfwDGZBK44BKYUd8SWT0F8A7dTeknWFCA_Y",
-    "auth": "uAiWLKRWoSJfyyhvBC8-4A"
+    "p256dh": "BN9cFp1A6txR7T8AzmqTHlKIFtDga4m6g6DvjXcTyuFuhedOCPxa4bl6ImZO3RTTsyNX-2fwpHWZ57eAm0ex_ic",
+    "auth": "KBjg-4pEYcZPTs6X1Z1KWw"
   }
 }
-
-const fix = {
-  "endpoint": "https://fcm.googleapis.com/fcm/send/dnkeliUP2o8:APA91bHoO-cnXSPhtx6Dh33_rZH9OrkdEfCar65chbsCTZ3K2tkqb4S19WxcFPfNOetacdl262dSaw99vxJlN2hD2t6hmlTeklWpTwTYNA1cLqKDxLmmPFX_Eo3ritw3YoZ43RLxhYdM",
-  "expirationTime": null,
-  "keys": {
-    "p256dh": "BPtmUg77eM-OW779A2mr0GAJTI0WmUdmnC6Z3wOUkN3JAEMpkwIInB1zlavyDHmnfrFIyZzBm0K0DNquPmENqwM",
-    "auth": "8_9aoDUdIDWPP3oOvWr8wA"
-  }
-};
 
 webpush.sendNotification(pushSubscription, "Your Push Payload Text");
