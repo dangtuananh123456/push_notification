de9175579b3fb1ae4ab735e952960e31a59aa9e9
21120408 <dangtuananh3012003@gmail.com>
2024-11-29 14:42:18 +0700

    thêm config cho popup thông báo phải được tắt bởi người dùng

--- sw.js
+++ sw.js
@@ -1,19 +1,13 @@
-const title = 'Actions Notification';
+const title = 'Thông báo giao việc hằng ngày';
 
 const options = {
   body: 'Vui lòng hoàn tất giao việc và chuyển Phiếu phân công công việc tổ sang trạng thái "Chờ duyệt" trước 15h hôm nay để đảm bảo tiến độ công việc.',
   image: 'https://images.pexels.com/photos/1001682/pexels-photo-1001682.jpeg?cs=srgb&dl=pexels-kellie-churchman-371878-1001682.jpg&fm=jpg',
   icon: 'https://images.pexels.com/photos/1001682/pexels-photo-1001682.jpeg?cs=srgb&dl=pexels-kellie-churchman-371878-1001682.jpg&fm=jpg',
   badge: 'https://images.pexels.com/photos/1001682/pexels-photo-1001682.jpeg?cs=srgb&dl=pexels-kellie-churchman-371878-1001682.jpg&fm=jpg',
-  // actions: [
-  //   {
-  //     action: 'reply',
-  //     type: 'text',
-  //     title: 'Reply',
-  //     icon: 'https://images.pexels.com/photos/1001682/pexels-photo-1001682.jpeg?cs=srgb&dl=pexels-kellie-churchman-371878-1001682.jpg&fm=jpg',
-  //   }
-  // ],
+  requireInteraction: true,
 };
+
 self.addEventListener('push', () => {
   self.registration.showNotification(title, options);
 });
@@ -21,7 +15,6 @@ self.addEventListener('push', () => {
 
 self.addEventListener("notificationclick", event => {
   console.log({event})
-
 })
 
 self.addEventListener("reply", event => {
